12ea1eb12f692003089670371b85e10f0c99b547
tkianai <tkianai@163.com>
2019-07-31 19:54:39 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tkcv",
+    version="0.1",
+    packages=find_packages(),
+    author="tkianai",
+    author_email="tkianai@163.com",
+    maintainer="tkianai",
+    url="https://github.com/tkianai/tk-cv",
+    description="The most commonly useful tools for computer vision, including transforms, annotation, traditional graphic operation and so on.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license=u"MIT License",
+    install_requires=[
+        'opencv-python>=4.1.0.25',
+        'scikit-image>=0.15.0', 
+    ],
+)
