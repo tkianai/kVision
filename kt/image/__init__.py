bca0c269a12fb40f87e74ea94c90db1d5df22803
tkianai <tkianai@163.com>
2019-08-02 12:15:26 +0800

    add blurry detection(toy) and imagehash tool

--- kt/image/__init__.py
+++ kt/image/__init__.py
@@ -1,2 +1,4 @@
 
 from .utils import image_format_check
+from .preprocess import is_blurry_by_gradient
+from .preprocess import get_image_hashcode
--- kt/image/config.py
+++ kt/image/config.py
@@ -1,5 +1,13 @@
 
+import imagehash
+import functools
 
 class ImageConf(object):
     
     _VALID_IMAGE_SUFFIXES = ['png', 'jpg', 'bmp']
+    _IMAGE_HASH_MODE = {
+        'a': imagehash.average_hash,  # average hash
+        'p': imagehash.phash,         # perception hash
+        'd': imagehash.dhash,         # difference hash
+        'w': imagehash.whash          # wavelet hash
+    }
