4da354387afbc50c8b499fa276582f4faaa84770
tkianai <tkianai@163.com>
2019-08-06 10:43:59 +0800

    fix import order bug

--- kt/image/__init__.py
+++ kt/image/__init__.py
@@ -3,5 +3,5 @@ from .utils import image_format_check
 from .preprocess import is_blurry_by_gradient
 from .preprocess import get_image_hashcode
 from .preprocess import get_md5_code
-from .merge import make_overlay
 from .io import imread
+from .merge import make_overlay
