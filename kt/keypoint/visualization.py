965fd7dc48ed34ce330a5196a9b8fc0888f7c0be
tkianai <tkianai@163.com>
2019-08-08 13:57:33 +0800

    expand keypoint skeleton definition range

--- kt/keypoint/visualization.py
+++ kt/keypoint/visualization.py
@@ -11,8 +11,10 @@ def show_keypoints_over_image(
     img, 
     points, 
     skeletons=None,
-    radius=3,
-    color=(0, 255, 0),
+    point_radius=3,
+    point_color=(0, 255, 0),
+    line_width=2,
+    line_color=None,
 ):
     """Plot keypoints on image
     
@@ -22,8 +24,10 @@ def show_keypoints_over_image(
     
     Keyword Arguments:
         skeletons {list of list} -- point collection (default: {None})
-        radius {int} -- radius of keypoint (default: {3})
-        color {tuple} -- keypoint color to show (default: {(0, 255, 0)})
+        point_radius {int} -- radius of keypoint (default: {3})
+        point_color {tuple} -- keypoint color to show (default: {(0, 255, 0)})
+        line_width {int} -- line width(default: {2})
+        line_color {tuple} -- line color to show (default: {None})
     
     Returns:
         img -- numpy image[opencv]
@@ -39,18 +43,22 @@ def show_keypoints_over_image(
     for point in points:
         # point: [width(x), height(y), visible]
         if point[2] > 0:
-            img = cv2.circle(img, (int(point[0]), int(point[1])), radius, color,-1)
+            img = cv2.circle(img, (int(point[0]), int(point[1])), point_radius, point_color,-1)
     
     
     # plot skeletons
     if skeletons is not None:
         for skeleton in skeletons:
-            p1_v, p2_v = points[skeleton[0]][2], points[skeleton[1]][2]
-            if p1_v > 0 and p2_v > 0:
-                line_color = np.random.randint(256, size=3).tolist()
-                p1 = (int(points[skeleton[0]][0]), int(points[skeleton[0]][1]))
-                p2 = (int(points[skeleton[1]][0]), int(points[skeleton[1]][1]))
-                img = cv2.line(img, p1, p2, line_color, 2)
+            for i in range(len(skeleton) - 1):
 
+                p1_v, p2_v = points[skeleton[i]][2], points[skeleton[i + 1]][2]
+                if p1_v > 0 and p2_v > 0:
+                    if line_color is None:
+                        _line_color = np.random.randint(256, size=3).tolist()
+                    else:
+                        _line_color = line_color
+                    p1 = (int(points[skeleton[i]][0]), int(points[skeleton[i]][1]))
+                    p2 = (int(points[skeleton[i + 1]][0]), int(points[skeleton[i + 1]][1]))
+                    img = cv2.line(img, p1, p2, _line_color, line_width)
 
     return img
