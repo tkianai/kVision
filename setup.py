867b86c55759132fabd05c3683649c394b7f11b0
tkianai <tkianai@163.com>
2019-08-08 11:36:17 +0800

    revise setup file

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     author="tkianai",
     author_email="tkianai@163.com",
     maintainer="tkianai",
-    url="https://github.com/tkianai/tk-cv",
+    url="https://github.com/tkianai/tkVision",
     description="The most commonly useful tools for computer vision, including transforms, annotation, traditional graphic operation and so on.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=u"MIT License",
     install_requires=[
         'opencv-python>=4.1.0.25',
-        'scikit-image>=0.15.0', 
+        'scikit-image', 
     ],
 )
